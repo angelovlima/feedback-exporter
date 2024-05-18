6535dedd4a071a10d77ffd4f5dcf2b258531e902
angelovlima <angelovictor2001@hotmail.com>
2024-05-18 18:28:12 -0300

    Spring Boot project

---
+++ src/main/java/com/fiap/api/feedbackexporter/FeedbackExporterApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.api.feedbackexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackExporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/api/feedbackexporter/FeedbackExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.api.feedbackexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
